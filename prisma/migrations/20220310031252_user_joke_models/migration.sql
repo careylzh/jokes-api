619591126ea6f1e5f4d14c6105c37f7b930488f7
Carey Lzh <lzh.carey@gmail.com>
2022-03-10 12:39:04 +0800

    feat(prisma): ran migrations on shadow db
    Co-authored-by: Anirudh181001 <Anirudh181001@users.noreply.github.com>

---
+++ prisma/migrations/20220310031252_user_joke_models/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Joke" (
+    "id" TEXT NOT NULL,
+    "text" VARCHAR NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Joke_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Joke" ADD CONSTRAINT "Joke_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
